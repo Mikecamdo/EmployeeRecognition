6c15bfa8a86fd298abb280c9d1dbfee0fe503e1f
Mikecamdo <mcdoherty@smu.edu>
2023-07-09 18:26:09 -0500

    Added landing-page
    Created a landing-page component, and added a template for a popup menu

--- Web/ClientApp/src/app/app.module.ts
+++ Web/ClientApp/src/app/app.module.ts
@@ -7,20 +7,25 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent
+    HomeComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
+      { path: '', component: LandingPageComponent, pathMatch: 'full' },
       { path: 'home', component: HomeComponent, pathMatch: 'full' },
-    ])
+    ]),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
