7c88dfbff1a8e51abc0a7ab0c7ee5c2faf17e232
Mikecamdo <mcdoherty@smu.edu>
2023-07-10 18:03:40 -0500

    Sign in and Sign up buttons
    Added the sign in and sign up buttons that activate the popup

--- Web/ClientApp/src/app/landing-page/landing-page.component.ts
+++ Web/ClientApp/src/app/landing-page/landing-page.component.ts
@@ -7,27 +7,10 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent {
-  closeResult = '';
   
   constructor(private modalService: NgbModal) {}
   
-  open(content: any) {
-    this.modalService.open(content,
-   {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = 
-         `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-  
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
+  open(content: any, label: string) {
+    this.modalService.open(content, {ariaLabelledBy: label});
   }
 }
