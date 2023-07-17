2649ae1d93b912a40d74a247718e66e351b38cde
Mikecamdo <mcdoherty@smu.edu>
2023-07-16 23:27:02 -0500

    Hide navbar on landing page
    Navbar should now show up on every page except for the landing page

--- Web/ClientApp/src/app/app.component.ts
+++ Web/ClientApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  showNavbar: boolean = false;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const currentRoute = this.router.url;  
+        this.showNavbar = currentRoute !== '/';
+      }
+    });
+  }
 }
