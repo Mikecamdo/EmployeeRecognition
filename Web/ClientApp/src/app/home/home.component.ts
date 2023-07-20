b7981df6734b2abf88efef5953ca99dc8fc45092
Mikecamdo <mcdoherty@smu.edu>
2023-07-19 22:56:27 -0500

    Viewing posted kudos
    Users can now view kudos that have been sent on the home page

--- Web/ClientApp/src/app/home/home.component.ts
+++ Web/ClientApp/src/app/home/home.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { Kudo, KudosService } from '../services/kudos.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  allKudos: Kudo[] = [];
 
   userId: string = '';
   userName: string = '';
 
-  constructor(private jwtHelper: JwtHelperService) {
+  constructor(private jwtHelper: JwtHelperService, private kudosService: KudosService) {
     this.jwtHelper = new JwtHelperService();
   }
 
@@ -21,5 +25,14 @@ export class HomeComponent implements OnInit {
 
     this.userId = decodedToken.id;
     this.userName = decodedToken.name;
+
+    this.kudosService.getAllKudos().subscribe({
+      next: kudos => {
+        this.allKudos = kudos.reverse();
+      },
+      error: error => {
+        console.log(error);
+      }
+    });
   }
 }
