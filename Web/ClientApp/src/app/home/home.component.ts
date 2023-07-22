5266fe6eb4f286ef873acd87e03bc327fa250ecd
Mikecamdo <mcdoherty@smu.edu>
2023-07-21 22:23:03 -0500

    Set up models for adding comment
    HTML input forms can now add comments to the database

--- Web/ClientApp/src/app/home/home.component.ts
+++ Web/ClientApp/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Kudo, KudosService } from '../services/kudos.service';
+import { Comment, CommentDto, CommentsService } from '../services/comments.service';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +12,15 @@ export class HomeComponent implements OnInit {
 
   allKudos: Kudo[] = [];
 
+  allComments: Comment[] = [];
+  commentMessage: string[] = [];
+
   userId: string = '';
   userName: string = '';
   userAvatar: string = '';
 
-  constructor(private jwtHelper: JwtHelperService, private kudosService: KudosService) {
+  constructor(private jwtHelper: JwtHelperService, private kudosService: KudosService,
+    private commentsService: CommentsService) {
     this.jwtHelper = new JwtHelperService();
   }
 
@@ -30,7 +35,39 @@ export class HomeComponent implements OnInit {
 
     this.kudosService.getAllKudos().subscribe({
       next: kudos => {
-        this.allKudos = kudos.reverse();
+        this.allKudos = kudos.reverse(); //FIXME I think I can reverse this in the backend database call
+        this.commentMessage = new Array(kudos.length).fill("");
+      },
+      error: error => {
+        console.log(error);
+      }
+    });
+
+    this.commentsService.getAllComments().subscribe({
+      next: comments => {
+        this.allComments = comments;
+      },
+      error: error => {
+        console.log(error);
+      }
+    });
+  }
+
+  activateButton(iteration: number): boolean {
+    return !this.commentMessage[iteration];
+  }
+
+  addComment(id: number, iteration: number): void {
+    let comment: CommentDto = {
+      kudoId: id,
+      senderId: this.userId,
+      message: this.commentMessage[iteration]
+    };
+
+    this.commentsService.addComment(comment).subscribe({
+      next: comment => {
+        this.commentMessage[iteration] = '';
+        this.allComments.push(comment); //FIXME make sure this works
       },
       error: error => {
         console.log(error);
