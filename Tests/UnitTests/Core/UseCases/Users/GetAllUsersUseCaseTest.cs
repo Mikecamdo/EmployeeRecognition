0297d09fd0418bfdfbae0df23464d670de391558
Mikecamdo <mcdoherty@smu.edu>
2023-07-30 22:17:14 -0500

    GetAllUsersUseCase Unit Tests
    Added a unit test for the use case that gets all users

---
+++ Tests/UnitTests/Core/UseCases/Users/GetAllUsersUseCaseTest.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeRecognition.Core.Entities;
+using EmployeeRecognition.Core.Interfaces.Repositories;
+using EmployeeRecognition.Core.UseCases.Users.GetAllUsers;
+using Moq;
+
+namespace EmployeeRecognition.Tests.UnitTests.Core.UseCases.Users;
+
+public class GetAllUsersUseCaseShould : MockDataSetup
+{
+    private readonly GetAllUsersUseCase _useCase;
+    private readonly Mock<IUserRepository> _mockUserRepo;
+
+    public GetAllUsersUseCaseShould()
+    {
+        _mockUserRepo = new Mock<IUserRepository>();
+        _useCase = new GetAllUsersUseCase(_mockUserRepo.Object);
+    }
+
+    [Fact]
+    public async void GetAllUsers_ReturnAllUsers()
+    {
+        //Arrange
+        var moqUsers = CreateMockUserList();
+        _mockUserRepo
+            .Setup(x => x.GetUsersAsync())
+            .Returns(Task.FromResult(moqUsers));
+        
+        //Act
+        IEnumerable<User> result = await _useCase.ExecuteAsync();
+
+        //Assert
+        foreach(var user in result)
+        {
+            Assert.IsType<User>(user);
+        }
+    }
+}
