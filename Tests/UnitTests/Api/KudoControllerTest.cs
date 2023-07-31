12034f251bb8acaee91c0fd401518b69961e5d5d
Mikecamdo <mcdoherty@smu.edu>
2023-07-31 17:54:31 -0500

    GetKudosBySenderId Unit Tests
    Added unit tests for getting kudos by their sender id

--- Tests/UnitTests/Api/KudoControllerTest.cs
+++ Tests/UnitTests/Api/KudoControllerTest.cs
@@ -3,6 +3,7 @@ using EmployeeRecognition.Api.Models;
 using EmployeeRecognition.Core.Entities;
 using EmployeeRecognition.Core.UseCases.Kudos.AddKudo;
 using EmployeeRecognition.Core.UseCases.Kudos.DeleteKudo;
+using EmployeeRecognition.Core.UseCases.Kudos.GetKudosBySenderId;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -140,5 +141,53 @@ public class KudoControllerShould : KudoControllerSetup
 
         var returnValue = okResult.Value;
         returnValue.Should().NotBeNull();
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void GetKudosBySenderId(bool senderExists)
+    {
+        //Arrange
+        if (senderExists)
+        {
+            _getKudosBySenderIdUseCase
+                .Setup(x => x.ExecuteAsync(It.IsAny<string>()))
+                .Returns((string y) => Task.FromResult<GetKudosBySenderIdResponse>(new GetKudosBySenderIdResponse.Success(
+                    KudoModelConverter.ToModel(CreateMockKudoList().Where(z => z.SenderId == y).ToList()))));
+        } else
+        {
+            _getKudosBySenderIdUseCase
+                .Setup(x => x.ExecuteAsync(It.IsAny<string>()))
+                .Returns((string y) => Task.FromResult<GetKudosBySenderIdResponse>(new GetKudosBySenderIdResponse.UserNotFound()));
+        }
+        string request = "a1b1bef1-1426-43cc-bcd9-d8425fe6e8e3";
+
+        //Act
+        var ctrl = CreateKudoController();
+        var result = ctrl.GetKudosBySenderId(request).Result;
+
+        //Assert
+        if (senderExists)
+        {
+            result.Should().BeOfType<OkObjectResult>();
+
+            var okResult = result as OkObjectResult;
+            okResult.Should().NotBeNull();
+
+            var returnValue = okResult.Value;
+            returnValue.Should().NotBeNull();
+        }
+        else
+        {
+            result.Should().BeOfType<BadRequestObjectResult>();
+
+            var badRequestResult = result as BadRequestObjectResult;
+            badRequestResult.Should().NotBeNull();
+
+            var returnValue = badRequestResult.Value;
+            returnValue.Should().NotBeNull();
+            Assert.Equal("A User with the given SenderId was not found", returnValue);
+        }
     }
 }
