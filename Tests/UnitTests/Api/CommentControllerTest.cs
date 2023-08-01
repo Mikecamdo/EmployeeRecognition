e61abb01c2c438a8b6a35914b3cedd0e69625b47
Mikecamdo <mcdoherty@smu.edu>
2023-07-31 20:39:54 -0500

    GetCommentsByKudoId Unit Tests
    Added unit tests for CommentController's GetCommentsByKudoId Function

--- Tests/UnitTests/Api/CommentControllerTest.cs
+++ Tests/UnitTests/Api/CommentControllerTest.cs
@@ -3,6 +3,7 @@ using EmployeeRecognition.Api.Models;
 using EmployeeRecognition.Core.Entities;
 using EmployeeRecognition.Core.UseCases.Comments.AddComment;
 using EmployeeRecognition.Core.UseCases.Comments.DeleteComment;
+using EmployeeRecognition.Core.UseCases.Comments.GetCommentsByKudoId;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -152,5 +153,52 @@ public class CommentControllerTest : CommentControllerSetup
 
         var returnValue = okResult.Value;
         returnValue.Should().NotBeNull();
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void GetCommentsByKudoId(bool kudoExists)
+    {
+        //Arrange
+        if (kudoExists)
+        {
+            _getCommentsByKudoIdUseCase
+                .Setup(x => x.ExecuteAsync(It.IsAny<int>()))
+                .Returns((int y) => Task.FromResult<GetCommentsByKudoIdResponse>(new GetCommentsByKudoIdResponse.Success(
+                    CommentModelConverter.ToModel(CreateMockCommentList().Where(z => z.KudoId == y).ToList()))));
+        } else
+        {
+            _getCommentsByKudoIdUseCase
+                .Setup(x => x.ExecuteAsync(It.IsAny<int>()))
+                .Returns((int y) => Task.FromResult<GetCommentsByKudoIdResponse>(new GetCommentsByKudoIdResponse.KudoNotFound()));
+        }
+        var request = 1;
+
+        //Act
+        var ctrl = CreateCommentController();
+        var result = ctrl.GetCommentsByKudoId(request).Result;
+
+        //Assert
+        if (kudoExists)
+        {
+            result.Should().BeOfType<OkObjectResult>();
+
+            var okResult = result as OkObjectResult;
+            okResult.Should().NotBeNull();
+
+            var returnValue = okResult.Value;
+            returnValue.Should().NotBeNull();
+        } else
+        {
+            result.Should().BeOfType<BadRequestObjectResult>();
+
+            var badRequestResult = result as BadRequestObjectResult;
+            badRequestResult.Should().NotBeNull();
+
+            var returnValue = badRequestResult.Value;
+            returnValue.Should().NotBeNull();
+            Assert.Equal("A Kudo with the given KudoId was not found", returnValue);
+        }
     }
 }
