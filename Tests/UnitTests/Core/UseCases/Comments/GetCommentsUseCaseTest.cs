df14625d313f8c083d433d508c9b0d2da83ed5a4
Mikecamdo <mcdoherty@smu.edu>
2023-07-31 21:16:15 -0500

    GetCommentsUseCase Unit Test
    Added a unit test for getting all comment's use case

---
+++ Tests/UnitTests/Core/UseCases/Comments/GetCommentsUseCaseTest.cs
@@ -0,0 +1,38 @@
+﻿using EmployeeRecognition.Core.Entities;
+using EmployeeRecognition.Core.Interfaces.Repositories;
+using EmployeeRecognition.Core.UseCases.Comments.GetComments;
+using Moq;
+
+namespace EmployeeRecognition.Tests.UnitTests.Core.UseCases.Comments;
+
+public class GetCommentsUseCaseShould : MockDataSetup
+{
+    private readonly GetCommentsUseCase _useCase;
+    private readonly Mock<ICommentRepository> _mockCommentRepo;
+
+    public GetCommentsUseCaseShould()
+    {
+        _mockCommentRepo = new Mock<ICommentRepository>();
+        _useCase = new GetCommentsUseCase(_mockCommentRepo.Object);
+    }
+
+    [Fact]
+    public async void GetComments_ReturnComments()
+    {
+        //Arrange
+        var moqComments = CreateMockCommentList();
+
+        _mockCommentRepo
+            .Setup(x => x.GetCommentsAsync())
+            .Returns(Task.FromResult(moqComments));
+
+        //Act
+        IEnumerable<Comment> result = await _useCase.ExecuteAsync();
+
+        //Assert
+        foreach(var comment in result)
+        {
+            Assert.IsType<Comment>(comment);
+        }
+    }
+}
